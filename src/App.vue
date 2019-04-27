ab267f30b1d39f11f6a5b0490a11049621974854
student19 <student19@pcoip.ki.agh.edu.pl>
2019-04-27 14:21:02 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+    <h1>elo</h1>
+    
+    <!--  comment -->
+    
+    <p> {{ email }} </p>
+   
+    <div v-if="loggedInVar">
+    <p> zalogowany jako {{ email }} </p>
+    <button @click="loggedOut">wyloguj</button>
+    
+    </div>
+    <div v-else>
+    <input type="text" v-model="email">
+    <button @click="loggedIn">zaloguj</button>
+    
+    <div v-if="email.length < 10">Ale masz krotki adres!</div>
+    <div v-else-if="email.length < 15">Twoj adres e-mail jest w sam raz.</div>
+    <div v-else>Twoj adres e-mail jest stanowczo za dlugi.</div>
+    <p> {{ email }} </p>
+    
+    </div>
+</div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	data() {
+		  return {
+		    email: 'jacek@jacekm.com',
+		    password: '',
+		    loggedInVar: false
+		  };
+		},
+	
+	methods: {
+		  alertMyEmail() {
+		    alert(this.email);
+		  },
+		  
+		  loggedIn() {
+
+			    this.loggedInVar = true
+			  },
+		  loggedOut(){
+				  this.loggedInVar = false
+			  }
+		}
+		
+	
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
