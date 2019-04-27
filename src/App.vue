4015dcdaf02076a1dd07a8269115775f4af0116b
student19 <student19@pcoip.ki.agh.edu.pl>
2019-04-27 14:24:19 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,29 @@
     <p> {{ email }} </p>
    
     <div v-if="loggedInVar">
-    <p> zalogowany jako {{ email }} </p>
-    <button @click="loggedOut">wyloguj</button>
-    
+	    <p> zalogowany jako {{ email }} </p>
+	    <button @click="loggedOut">wyloguj</button>
+	    
     </div>
     <div v-else>
-    <input type="text" v-model="email">
-    <button @click="loggedIn">zaloguj</button>
-    
-    <div v-if="email.length < 10">Ale masz krotki adres!</div>
-    <div v-else-if="email.length < 15">Twoj adres e-mail jest w sam raz.</div>
-    <div v-else>Twoj adres e-mail jest stanowczo za dlugi.</div>
-    <p> {{ email }} </p>
+	    <input type="text" v-model="email">
+	    <button @click="loggedIn">zaloguj</button>
+	    
+	    <div v-if="email.length < 10">Ale masz krotki adres!</div>
+	    <div v-else-if="email.length < 15">Twoj adres e-mail jest w sam raz.</div>
+	    <div v-else>Twoj adres e-mail jest stanowczo za dlugi.</div>
+	    <p> {{ email }} </p>
     
     </div>
+    
 </div>
   
   
 </template>
 
 <script>
+
+import "milligram";
 
 
 
@@ -45,7 +48,6 @@ export default {
 		  },
 		  
 		  loggedIn() {
-
 			    this.loggedInVar = true
 			  },
 		  loggedOut(){
