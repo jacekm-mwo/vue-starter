edf46ac22690241491f0888338ec30b20b89d7a9
student19 <student19@pcoip.ki.agh.edu.pl>
2019-04-27 15:01:46 +0200

    a

---
+++ src/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+   <div>
+     <form @submit.prevent>
+       <label>Zaloguj siê e-mailem</label>
+       <input type="email" v-model="email">
+       <button @click="enter()">Wchodzê</button>
+    </form>
+   </div>
+</template>
+
+<script>
+export default {
+ data() {
+     return {
+         email: ''
+     }
+ },
+ methods: {
+     enter() {
+         this.$emit('login', this.email);
+     }
+ }
+}
+</script>
